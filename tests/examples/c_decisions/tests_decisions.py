b4c23b57775479e29187bd95ce58e95f6000e387
tylerallman9142 <tyler.allman@g.austincc.edu>
2025-02-14 03:44:41 +0000

    Hw2 Sum/Return Functions

--- tests/examples/c_decisions/tests_decisions.py
+++ tests/examples/c_decisions/tests_decisions.py
@@ -7,3 +7,19 @@ class Test_Config(unittest.TestCase):
     def test_configuration(self):
         self.assertEqual(True, test_config())
 
+    def test_and_truth_table(self):
+
+        self.assertEqual(False and False, False)
+        self.assertEqual(True and False, False)
+        self.assertEqual(False and True, False)
+        self.assertEqual(True and True, True)
+
+    def test_or_truth_table(self):
+
+        self.assertEqual(False or False, False)
+        self.assertEqual(True or False, True)
+        self.assertEqual(False or True, True)
+        self.assertEqual(True or True, True)
+    
+
+
