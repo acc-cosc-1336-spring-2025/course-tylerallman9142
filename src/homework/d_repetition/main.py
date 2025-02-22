5fa23446870d0740fff146f9380a629bfe3bbfb0
tylerallman9142 <tyler.allman@g.austincc.edu>
2025-02-22 19:00:15 +0000

    HW3 For Range/While Function

--- src/homework/d_repetition/main.py
+++ src/homework/d_repetition/main.py
@@ -1,1 +1,27 @@
 #
+from unittest import result
+import repetition
+
+def main():
+    repetition.get_factorial(1)
+    repetition.sum_odd_numbers(1)
+    while True:
+        print("Homework 3 Menu")
+        print("1 - Factorial")
+        print("2 - Sum odd numbers")
+        print("3 - Exit")
+
+        option = input("Select an Option: ")
+
+        if option == '1':
+            num = int(input("Enter a number: "))
+            print("The factorial of", num, "is", repetition.get_factorial(num))
+        elif option == '2':
+            num = int(input("Enter a number: "))
+            print("The sum of odd numbers up to", num, "is", repetition.sum_odd_numbers(num))
+        elif option == '3':
+            print("Do you wish to continue?")
+
+        
+main()
+    
--- src/homework/d_repetition/repetition.py
+++ src/homework/d_repetition/repetition.py
@@ -1,1 +1,20 @@
 #
+
+def get_factorial(num):
+    factorial = 1
+
+    for val in range(1, num + 1):
+        factorial *= val
+
+    return factorial
+
+def sum_odd_numbers(num):
+
+    sum = 0
+    n = 1
+
+    while(n <= num):
+        sum += n
+        n += 2
+
+    return sum
