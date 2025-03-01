8868cf68a0f3ec106e1548be168e3694eb96ef39
tylerallman9142 <tyler.allman@g.austincc.edu>
2025-03-01 04:02:48 +0000

    HW 4 Return Value Functions

--- src/homework/e_functions/main.py
+++ src/homework/e_functions/main.py
@@ -1,1 +1,18 @@
 #
+
+import value_return
+
+def main():
+    result = value_return.get_hour(3800)
+
+    print (result)
+
+    result = value_return.get_minutes(3800)
+
+    print (result)
+
+    result = value_return.get_seconds (3800)
+
+    print (result)
+
+main()
--- src/homework/e_functions/value_return.py
+++ src/homework/e_functions/value_return.py
@@ -1,1 +1,16 @@
 #
+
+import datetime
+
+
+def get_hour(epoch_seconds):
+    dt_object = datetime.datetime.fromtimestamp(epoch_seconds)
+    return dt_object.hour
+
+def get_minutes(epoch_seconds):
+    dt_object = datetime.datetime.fromtimestamp(epoch_seconds)
+    return dt_object.minute
+
+def get_seconds(epoch_seconds):
+    dt_object = datetime.datetime.fromtimestamp(epoch_seconds)
+    return dt_object.second
